92e710235a735a80dbe8d1dc683f197a852aad4b
Benny Steve <bennysteve2005@gmail.com>
2026-03-09 03:42:38 +0000

    created basic pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,2 +1,14 @@
 import sys
-print("hi")
+import pandas as pd
+
+print("arguments",sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day":[1,2],"num_passengers":[3,4]})
+df['month'] = month
+print(df)
+
+df.to_parquet(f"output_{month}.parquet")
+
+print(f'hello pipeline, month={month}')
